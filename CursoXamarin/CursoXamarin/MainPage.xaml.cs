618064311c453100e65378986d3a6efbeb707ed3
Zincri <zincri_mlz@hotmail.com>
2019-04-16 16:32:10 -0500

    jhoana
    16 de abril-login

--- CursoXamarin/CursoXamarin/MainPage.xaml.cs
+++ CursoXamarin/CursoXamarin/MainPage.xaml.cs
@@ -12,6 +12,27 @@ namespace CursoXamarin
         public MainPage()
         {
             InitializeComponent();
+            usuario.Text = String.Empty;
+            password.Text = String.Empty;
+
+        }
+
+            void Login(object sender, System.EventArgs e)
+            {
+
+                App.Current.MainPage.DisplayAlert("login", "click en login " + usuario.Text, "Bienvenido");
+                usuario.Text = String.Empty;
+                password.Text = String.Empty;
+
+            }
+
+            void Register(object sender, System.EventArgs e)
+            {
+                App.Current.MainPage.DisplayAlert("Register", "Click en register " + usuario.Text, "Registrate");
+
+            }
         }
     }
-}
+
+
+
