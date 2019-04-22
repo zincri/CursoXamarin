9c31dc37958d40108d54fe13e2e41bd5d8fe8821
Zincri <zincri_mlz@hotmail.com>
2019-04-22 16:46:38 -0500

    Zincri
    Zincri MVVM

--- CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
+++ CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
@@ -9,11 +9,24 @@
             get;
             set;
         }
+        public string Password
+        {
+            get;
+            set;
+        }
+        public bool IsEnabled 
+        {
+            get;
+            set;
+        }
+
         #endregion
         #region Constructors
         public LoginViewModel()
         {
             Usuario = "zincri";
+            Password = "123456";
+            IsEnabled = false;
         }
         #endregion
     }
