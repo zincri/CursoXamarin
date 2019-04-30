7a8316c679e5920f7360be7dad86491317d9f5fc
ANTONIOPEREZ97 <Tony9706-@hotmail..com>
2019-04-29 20:30:38 -0500

    Command
    Implemtacion de Command con async

--- CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
+++ CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
@@ -1,18 +1,42 @@
 ﻿
 namespace CursoXamarin.ViewModels
 {
+    using CursoXamarin.Views;
+    using GalaSoft.MvvmLight.Command;
     using System;
+    using System.Windows.Input;
+
     public  class LoginViewModel
     {
         #region Properties
         public String Usuario { get; set; }
+        public String password { get; set; }
+        public bool IsEnabled { get; set; }
         #endregion
+
+        #region Comands
+           
+        public ICommand LoginComand { get { return new RelayCommand(LoginMethod); } } // Relay es una clase
+private  async void LoginMethod()
+        {
+
+
+            //App.Current.MainPage.DisplayAlert("Login","Click en Login" ,"Ok");
+            //IsEnabled = false;
+
+            await App.Current.MainPage.Navigation.PushAsync(new TwoPage());  // Para lanzar otra pagina 
+        }
+
+        #endregion
+
+
 
         public LoginViewModel()
         {
 
-            this.Usuario = "Antonio Perez";
-
+            this.Usuario = "Antonio ";
+            this.password = "1234";
+            IsEnabled=true;
         }
 
     }
