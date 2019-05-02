3b7eb395be638eaf22bbec0c288d47216375898d
ANTONIOPEREZ97 <Tony9706-@hotmail..com>
2019-05-01 19:26:43 -0500

    Uso de Brackquet
    Uso de CallerMember

--- CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
+++ CursoXamarin/CursoXamarin/ViewModels/LoginViewModel.cs
@@ -4,20 +4,45 @@ namespace CursoXamarin.ViewModels
     using CursoXamarin.Views;
     using GalaSoft.MvvmLight.Command;
     using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
     using System.Windows.Input;
 
-    public  class LoginViewModel
+    #region Methods
+    public class LoginViewModel : INotifyPropertyChanged
+    #endregion
     {
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region Vars
+        private bool _isEnabled;
+
+        #endregion
+
+
+
+
+
         #region Properties
         public String Usuario { get; set; }
         public String password { get; set; }
-        public bool IsEnabled { get; set; }
+        public bool IsEnabled { get { return _isEnabled; } set { _isEnabled = value; OnPropertyChanged(); } }
+
+
         #endregion
 
         #region Comands
-           
+
         public ICommand LoginComand { get { return new RelayCommand(LoginMethod); } } // Relay es una clase
-private  async void LoginMethod()
+        #endregion 
+
+
+
+        #region Methods
+        private async void LoginMethod()
         {
 
 
@@ -25,19 +50,39 @@ private  async void LoginMethod()
             //IsEnabled = false;
 
             await App.Current.MainPage.Navigation.PushAsync(new TwoPage());  // Para lanzar otra pagina 
+            IsEnabled = false;
         }
 
         #endregion
 
 
 
+        #region Constructor
         public LoginViewModel()
         {
 
             this.Usuario = "Antonio ";
             this.password = "1234";
-            IsEnabled=true;
+            IsEnabled = true;
         }
+
+        // Para no esceficar lo que va ha llamar
+        //Brackquets especificamos que tiene que pasar por CallerMember
+        private void OnPropertyChanged([CallerMemberName] String PropertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+
+                PropertyChanged(this, new PropertyChangedEventArgs(PropertyName));
+
+
+            }
+
+
+
+
+        }
+        #endregion
 
     }
 }
